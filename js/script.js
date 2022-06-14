2d8c7c7cbefd2e6b5c78994c71ac5477fea86163
rehan1996 <s.rehanali96@gmail.com>
2022-06-14 18:18:17 +0200

    Task_Completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log('External script implemented successfully'); // Just for debugging, delete later
+function populateList() {
+  var ul = document.getElementById("mainList");
+  for (var i = 1; i <= 100; i++) {
+    var li = document.createElement("li");
+
+    ul.appendChild(li);
+    if (i % 5 == 0 && i % 3 == 0) {
+      li.innerHTML = "FizzBuzz";
+      li.setAttribute('class', 'fizzbuzz--text');
+    } else if (i % 3 == 0) {
+      li.innerHTML = "Fizz";
+      li.setAttribute('class', 'fizz--text');
+    } else if (i % 5 == 0) {
+      li.innerHTML = "Buzz";
+      li.setAttribute('class', 'buzz--text');
+    } else {
+      li.innerHTML = i;
+    }
+  }
+}
